7bce55e4ae9dc193fbe1d00be31bd2e3d22aeb74
TheophilusM <theotakumug@gmail.com>
2022-05-05 18:18:04 +0200

    rote

--- src/main/java/com/metbank/peyhub/controller/UserController.java
+++ src/main/java/com/metbank/peyhub/controller/UserController.java
@@ -3,6 +3,7 @@ package com.metbank.peyhub.controller;
 import com.metbank.peyhub.document.User;
 import com.metbank.peyhub.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
